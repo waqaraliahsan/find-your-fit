d157da87a12f75c659774b974b12b1022970eaa7
waqaraliahsan <93543855+waqaraliahsan@users.noreply.github.com>
2025-08-23 06:06:03 +0500

    Delete lib directory

--- lib/queries/mentors.ts
+++
@@ -1,7 +0,0 @@
-import { supabaseServerComponent } from "@/lib/supabase/server";
-export async function getMentors(){
-  const supabase = supabaseServerComponent();
-  const { data, error } = await supabase.from('profiles').select('*').eq('can_mentor',true);
-  if(error) throw error;
-  return data;
-}
